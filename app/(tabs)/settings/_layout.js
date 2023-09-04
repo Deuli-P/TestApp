527707484a1f3aff3b3c15b3971ca781d631a6d2
Antoniutti Pierre <pierre.antoniutti@gmail.com>
2023-09-04 11:59:53 +0200

    init

---
+++ app/(tabs)/settings/_layout.js
@@ -0,0 +1,9 @@
+import { Stack } from "expo-router";
+
+const Layout = () => {
+  return <Stack 
+  screenOptions={
+    {headerShown:false,
+    headerTitle:"Options"}}/>;
+};
+export default Layout;
---
+++ components/Details.js
@@ -0,0 +1,35 @@
+import { Stack, useLocalSearchParams, useRouter } from "expo-router";
+import { useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Details() {
+  const router : useRouter();
+  const params : useLocalSearchParams();
+  return (
+    <View>
+      <Stack.Screen
+        options={{
+          title: params.name,
+        }}
+      />
+      <View>
+        <Text style={styles.name}> Intégration {params.name}</Text>
+
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  name: {
+    fontSize: 20,
+    fontWeight: "bold",
+    textAlign: "center",
+    margin: 10,
+  },
+  text: {
+    fontSize: 16,
+    textAlign: "center",
+    margin: 10,
+  },
+});
